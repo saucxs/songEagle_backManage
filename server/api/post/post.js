f71c07e503c2eb1ef4410638fd1d3442f7094525
saucxs <saucxs@163.com>
2018-12-25 13:55:41 +0800

    系统配置接口联调，加入简单日志中间价logger

--- server/api/post/post.js
+++ server/api/post/post.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')();
 const post = require('./post.controller');
 const comment = require('./comment.controller');
+const system = require('./system.controller');
 
 router.get('/getPostList', post.getPostList);
 router.get('/getPost/:id', post.getPost);
@@ -10,8 +11,11 @@ router.get('/tag/:id', post.getPostsByTagId);
 router.get('/keyword/:keyword', post.getPostsByKeyword);
 
 router.get('/getLaboratory', post.getLaboratory);
+//system-config配置
+router.get('/getSystem', system.getSystem);
 
 router.post('/addComment', comment.addComment);
 router.get('/getCommentsByPostId/:postId', comment.getCommentsByPostId);
+
 
 module.exports = router;
--- server/app.js
+++ server/app.js
@@ -8,6 +8,9 @@ const session = require('koa-session2');
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
+const logger = require('koa-logger');
+
+app.use(logger())
 
 app.use(session({
   store: new Store(config.db.redis),
@@ -15,8 +18,10 @@ app.use(session({
 }));
 
 app.use(async(ctx, next) => {
+  ctx.set("Access-Control-Allow-Origin", ctx.request.header.origin);
+  ctx.set("Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE");
+  ctx.set("Access-Control-Allow-Headers", "x-requested-with, accept, origin, content-type");
   ctx.execSql = query;
-  ctx.set('Access-Control-Allow-Origin', config.accessControlAllowOrigin);
   await next();
 });
 
